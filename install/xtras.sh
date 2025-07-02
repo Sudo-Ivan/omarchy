86d904e137bb3b29079a59c3b633a53dd4e1462c
Ivan <ivan@quad4.io>
2025-07-02 18:54:22 -0500

    update

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,6 +1,3 @@
 yay -S --noconfirm --needed \
   obsidian onlyoffice-bin kdenlive \
   pinta strawberry
-
-# Copy over Omarchy applications
-source ~/.local/share/omarchy/bin/omarchy-sync-applications
