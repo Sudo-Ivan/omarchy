2cca84c35c7c2a1d053fd39a91d5942cca55a0cf
Ivan <ivan@quad4.io>
2025-07-02 23:12:00 -0500

    add flatpak

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,5 +1,5 @@
 yay -S --noconfirm --needed \
   obsidian onlyoffice-bin kdenlive \
-  pinta strawberry fuse2 libfido2
+  pinta strawberry fuse2 libfido2 flatpak
 
 source ~/.local/share/omarchy/bin/omarchy-sync-applications
