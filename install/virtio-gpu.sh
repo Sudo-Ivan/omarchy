e36f887459fe918f501c939e6499590858882457
Ivan <ivan@quad4.io>
2025-07-02 22:02:53 -0500

    remove

--- install/virtio-gpu.sh
+++
@@ -1,9 +0,0 @@
-yay -S --noconfirm --needed \
-  xf86-video-qxl spice-vdagent
-
-sudo systemctl enable --now spice-vdagentd
-
-if ! grep -q "virtio_gpu" /etc/mkinitcpio.conf; then
-  sudo sed -i '/^MODULES=/ s/)/ virtio_gpu virtio_pci virtio_blk virtio_scsi virtio_net)/' /etc/mkinitcpio.conf
-  sudo mkinitcpio -P
-fi
