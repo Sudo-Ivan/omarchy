b4fbd9047f44893c7018731601b6e77497b05790
Ivan <ivan@quad4.io>
2025-07-02 19:08:54 -0500

    Add virtio-gpu installation script

---
+++ install/virtio-gpu.sh
@@ -0,0 +1,10 @@
+yay -S --noconfirm --needed \
+  mesa lib32-mesa vulkan-mesa-layers lib32-vulkan-mesa-layers \
+  xf86-video-qxl spice-vdagent
+
+sudo systemctl enable --now spice-vdagentd
+
+if ! grep -q "virtio_gpu" /etc/mkinitcpio.conf; then
+  sudo sed -i '/^MODULES=/ s/)/ virtio_gpu virtio_pci virtio_blk virtio_scsi virtio_net)/' /etc/mkinitcpio.conf
+  sudo mkinitcpio -P
+fi
