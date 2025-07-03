84919f01745a2e7bbfe619cb17927bbf27a4d865
Ivan <ivan@quad4.io>
2025-07-02 21:29:38 -0500

    refine mirror ranking script to include fewer countries and reduce age parameter; add echo for testing mirrors

--- install/0-rank-mirrors.sh
+++ install/0-rank-mirrors.sh
@@ -1,5 +1,6 @@
 sudo pacman -S --needed --noconfirm reflector
 
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sudo reflector --country US,CA,GB,DE,FR,NL,SE,NO,DK,FI --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --country US,DE,NL --age 6 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo testing mirrors...
 sudo pacman -Sy
