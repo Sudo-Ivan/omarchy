ea09ec9e9e9d2099b256c5134cc821be40668abd
Ivan <ivan@quad4.io>
2025-07-05 11:20:33 -0500

    Conditionally install `plocate` based on the absence of `updatedb`.

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -2,5 +2,5 @@ yay -S --noconfirm --needed \
   wget curl unzip inetutils \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
-  man tldr less whois plocate \
+  man tldr less whois $(if ! command -v updatedb &> /dev/null; then echo "plocate"; fi) \
   alacritty
