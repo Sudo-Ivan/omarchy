29edca6add83b111bf0cd6ed98ba6df1ccd6f73d
Ivan <ivan@quad4.io>
2025-07-15 00:35:43 -0500

    feat(install): Add new packages to the xtras installation script.

--- install/xtras.sh
+++ install/xtras.sh
@@ -2,6 +2,7 @@ if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed \
     gnome-calculator \
     obsidian-bin onlyoffice-bin gnome-keyring \
+    strawberry fuse2 libfido2 flatpak
 fi
 
 # Copy over Omarchy applications
