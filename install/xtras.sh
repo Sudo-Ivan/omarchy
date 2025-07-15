a13e9c4b58acdf808df3ad1eb415d71fc0ffdf36
Ivan <ivan@quad4.io>
2025-07-15 00:29:32 -0500

    Refactor: Remove unused configurations and installation scripts
    Deletes various configuration files for applications like Typora, Lazygit, and Xournal++, and removes several installation scripts for development tools, Docker, printers, Ruby, and web applications. This streamlines the repository by removing components no longer needed for the core Omarchy setup.

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,10 +1,7 @@
 if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed \
     gnome-calculator \
-    signal-desktop spotify dropbox-cli zoom \
-    obsidian-bin typora libreoffice obs-studio kdenlive \
-    1password-beta 1password-cli gnome-keyring \
-    pinta xournalpp localsend-bin
+    obsidian-bin onlyoffice-bin gnome-keyring \
 fi
 
 # Copy over Omarchy applications
